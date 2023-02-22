fb19d0631efedae508da65821283466ca273b6a2
sho037 <103867061+sho037@users.noreply.github.com>
2023-02-22 23:10:05 +0900

    🎉 first commit

---
+++ docker/app/entrypoint.sh
@@ -0,0 +1,7 @@
+bundle install
+bundle update
+bundle exec rails db:create
+bundle exec rails db:migrate
+bundle exec rails db:seed
+rm -f tmp/pids/server.pid
+bundle exec rails s -p 3000 -b '0.0.0.0'
